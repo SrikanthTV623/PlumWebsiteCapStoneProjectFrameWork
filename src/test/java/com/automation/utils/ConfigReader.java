4ad97c66e1726f809de06e1e657d025bb7edcb18
Srikanth.TV <Srikanth.TV@ust.com>
2024-08-17 12:20:44 +0530

    base page commit

--- src/test/java/com/automation/utils/ConfigReader.java
+++ src/test/java/com/automation/utils/ConfigReader.java
@@ -9,12 +9,14 @@ public class ConfigReader {
     static Properties prop;
 
     public static void initConfig(){
+
         prop = new Properties();
         try {
             prop.load(new FileInputStream("src/test/resources/config/config.properties"));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
     }
 
     public static String getConfigValue(String key){
