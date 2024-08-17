bfb48e5824787ea1b41deda6af65910dd8a6fa22
Savita.RavindraKalla <Savita.RavindraKalla@ust.com>
2024-08-17 14:59:33 +0530

    login commit

--- src/test/java/com/automation/pages/HomePage.java
+++ src/test/java/com/automation/pages/HomePage.java
@@ -1,4 +1,25 @@
 package com.automation.pages;
 
-public class HomePage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends BasePage{
+
+    @FindBy(xpath = "//span[@class='flex']/img")
+    WebElement plumLogo;
+    @FindBy(id = "kp-login-button-header-logo")
+    WebElement loginBtn;
+
+    public void openWebsite() {
+
+        driver.get("https://plumgoodness.com/");
+    }
+
+    public boolean verifyHomePage() {
+        return plumLogo.isDisplayed();
+    }
+
+    public void clickOnLoginButton() {
+        loginBtn.click();
+    }
 }
---
+++ src/test/java/com/automation/steps/LoginSteps.java
@@ -0,0 +1,27 @@
+package com.automation.steps;
+
+import com.automation.pages.LoginPage;
+import com.automation.utils.ConfigReader;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+public class LoginSteps {
+    LoginPage loginPage=new LoginPage();
+    @Then("verify user is on login page")
+    public void verify_user_is_on_login_page() {
+        Assert.assertTrue(loginPage.verifyLoginPage());
+    }
+
+    @When("user enters a valid phone number {string} and OTP")
+    public void userEntersAValidPhoneNumberAndOTP(String phoneNo) {
+        loginPage.enterMobileNumber(ConfigReader.getConfigValue(phoneNo));
+    }
+
+    @Then("verify user is logged in")
+    public void verify_user_is_logged_in() {
+        
+    }
+
+
+}
