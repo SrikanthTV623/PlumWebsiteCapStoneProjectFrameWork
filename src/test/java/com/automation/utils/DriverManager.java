5f1266cc2df027becd9b274c00d95081842dcd0b
Srikanth.TV <Srikanth.TV@ust.com>
2024-08-21 11:48:36 +0530

    driver commit

--- src/test/java/com/automation/utils/DriverManager.java
+++ src/test/java/com/automation/utils/DriverManager.java
@@ -2,6 +2,7 @@ package com.automation.utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 
 import java.time.Duration;
@@ -11,8 +12,10 @@ public class DriverManager {
     static WebDriver driver;
 
     public static void createDriver(){
-        driver = new EdgeDriver();
-        //driver = new ChromeDriver();
+        //driver = new EdgeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--disable-notifications");
+        driver = new ChromeDriver(chromeOptions);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
     }
